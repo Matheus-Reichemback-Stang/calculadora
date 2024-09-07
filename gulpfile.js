d1b31d9491b24d38eb46e8182f806b4fbdf7d9c0
zkd <matheus.reichemback.stang@gmail.com>
2024-09-07 07:40:48 -0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+// Faz as Funções Gerais
+const gulp = require('gulp');
+// Minifica o HTML
+const htmlmin = require('gulp-htmlmin');
+// Compila e Minifica o SASS
+const sass = require('gulp-sass')(require('sass'));
+// Cria um Mapa de rota
+const sourcemaps = require('gulp-sourcemaps');
+// Minifica o JavaScript
+const uglify = require('gulp-uglify');
+
+const compileSass = () => {
+    return gulp.src('./src/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./map'))
+        .pipe(gulp.dest('./dist/styles'))
+}
+
+const compileJS = () => {
+    return gulp.src('./src/scripts/main.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/scripts'))
+}
+
+const compileHtml = () => {
+    return gulp.src('./src/index.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('./dist'))
+}
+
+
+
+exports.default = gulp.parallel(compileSass, compileJS)
+exports.watch = () => {
+    gulp.watch('./src/index.html', {ignoreInitial: false}, gulp.parallel(compileHtml))
+    gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, gulp.parallel(compileSass))
+    gulp.watch('./src/scripts/*.js', {ignoreInitial: false}, gulp.parallel(compileJS))
+}
